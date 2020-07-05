1e3c863c04ed24494b675988099fdf06914172fe
Artemis <artemisliuai@gmail.com>
2020-07-05 14:08:50 -0400

    changed logo and text

---
+++ pujee_is_frank/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="ServerLogo.png" className="App-logo" alt="logo" />
+        <p>
+          Welcome to Pujee is Frank
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
